980064891c28eed103f8487f1601c8b32bfe8d46
pot <pot@sennalabs.com>
2023-08-25 14:21:12 +0700

    docs(code-standard-ui-react): setup project structure

---
+++ Developer/development/code standard/ui/react.js/src/Root.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
+import App from "App";
+
+export function Root() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
